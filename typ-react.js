ac323a06fd229c5c5d255a70cb500bfbd062a09d
stronGui <1453543269@qq.com>
2020-09-11 00:52:42 +0800

    commit code

--- typ-react.js
+++ typ-react.js
@@ -1,45 +1,6 @@
 
 
 const RENDER_TO_DOM = Symbol('render to dom')
-
-class ElementWrapper {
-    constructor(type) {
-        this.root = document.createElement(type);
-    }
-    setAttribute(name,value) {
-        if(name.match(/^on([\s\S]+)/)){
-
-            this.root.addEventListener(RegExp.$1.replace(/^[\s\S]/,c => c.toLocaleLowerCase()),value);
-        }else{
-            if(name == 'className'){
-                this.root.setAttribute("class",value);
-            }
-            this.root.setAttribute(name,value)
-        }
-
-    }
-    appendChild(component) {
-        let range = document.createRange();
-        range.setStart(this.root,this.root.childNodes.length);
-        range.setEnd(this.root,this.root.childNodes.length);
-        component[RENDER_TO_DOM](range);
-    }
-    [RENDER_TO_DOM](range) {
-        range.deleteContents();
-        range.insertNode(this.root)
-    }
-}
-
-class TextWrapper {
-    constructor(content) {
-        this.root = document.createTextNode(content)
-    }
-    [RENDER_TO_DOM](range) {
-        range.deleteContents();
-        range.insertNode(this.root)
-    }
-}
-
 export class Component {
     constructor() {
         this.props = Object.create(null);
@@ -54,9 +15,73 @@ export class Component {
         this.children.push(component);
     }
 
+    get vdom(){
+        return this.render().vdom;
+    }
+
+
+
     [RENDER_TO_DOM](range) {
         this._range = range;
         this.render()[RENDER_TO_DOM](range);
+    }
+
+    update() {
+
+        let isSamme = (oldNode,newNode) => {
+            if(oldNode.type !== newNode.type)
+                return false;
+
+            for(let name in newNode.props){
+                if(newNode.props[name] !== oldNode.props[name]){
+                    return false;
+                }
+            }
+            if(Object.keys(oldNode.props).length> Object.keys(newNode.props).length)
+                return false;
+
+            if(newNode.type === '#text'){
+                if(newNode.content !== oldName.content)
+                    return false;
+            }
+            return false;
+        }
+        let update = (oldNode,newNode) =>{
+            if(!isSamme(oldNode,newNode)){
+                newNode[RENDER_TO_DOM](oldNode._range);
+                return;
+            }
+            newNode._range = oldNode._range;
+
+            let newChildren = newNode.vchildren;
+            let oldChildren = oldNode.vchildren;
+
+            if(!newChildren || !newChildren.length){
+                return;
+            }
+
+            let tailRange = oldChildren[oldChildren.length -1]._range;
+
+
+            for(let i =0;i<newChildren.length; i++) {
+                let newChild = newChildren[i];
+                let oldChild = oldChildren[i];
+                if(i<oldChildren.length) {
+                    update(oldChild,newChild);
+                }else{
+                    let range = document.createRange();
+                    range.setStart(tailRange.endContainer,tailRange.endOffset);
+                    range.setEnd(tailRange.endContainer,tailRange.endOffset);
+                    newChild[RENDER_TO_DOM](range);
+                    tailRange = range;
+                }
+            }
+
+
+        }
+        let vdom = this.vdom;
+        update(this._vdom,vdom);
+        this._vdom = vdom;
     }
 
     rerender() {
@@ -90,6 +115,76 @@ export class Component {
         this.rerender();
     }
 
+}
+class ElementWrapper extends Component{
+    constructor(type) {
+        super(type);
+        this.type = type;
+        this.root = document.createElement(type);
+    }
+    get vdom() {
+        this.vchildren = this.children.map(child => child.vdom);
+        return this;
+    }
+    [RENDER_TO_DOM](range) {
+
+        let root = document.createElement(this.type);
+        for(let name in this.props){
+            let value = this.props[name]
+            if(name.match(/^on([\s\S]+)/)){
+
+                root.addEventListener(RegExp.$1.replace(/^[\s\S]/,c => c.toLocaleLowerCase()),value);
+            }else{
+                if(name == 'className'){
+                    root.setAttribute("class",value);
+                }
+                    root.setAttribute(name,value)
+            }
+        }
+
+        if(!this.vchildren)
+            this.vchildren = this.children.map(child => child.vdom);
+
+
+        for(let child of this.children){
+            let childRange = document.createRange();
+            childRange.setStart(root,root.childNodes.length);
+            childRange.setEnd(root,root.childNodes.length);
+            child[RENDER_TO_DOM](childRange);
+        }
+        replaceContent(range,root)
+        range.insertNode(root)
+    }
+}
+
+class TextWrapper extends Component{
+    constructor(content) {
+        super(content);
+        this.type = '#text';
+        this.content = content;
+    }
+    get vdom(){
+        return this;
+        // {
+        //     type: '#text',
+        //     content: this.content
+        // }
+    }
+    [RENDER_TO_DOM](range) {
+        this._range = range;
+        let root = document.createTextNode(this.content);
+
+        replaceContent(range,root)
+    }
+}
+
+function replaceContent(range,node) {
+    range.insertNode(node);
+    range.setStartAfter(node);
+    range.deleteContents();
+
+    range.setStartBefore(node);
+    range.setEndAfter(node);
 }
 
 
